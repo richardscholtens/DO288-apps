19cfd48f3dae18506f601987e2ab8cf0064d3b96
Student User <student@workstation.lab.example.com>
2023-04-11 08:57:48 -0400

    change

--- build-app/app.js
+++ build-app/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Simple app for the Building Applications Lab!\n');
+  res.send('Amazing app for the Building Applications Lab!\n');
 });
 
 app.listen(8080, function () {
-  console.log('Simple app for the Building Applications Lab!');
+  console.log('Amazing app for the Building Applications Lab!');
 });
 
